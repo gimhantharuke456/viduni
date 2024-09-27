02b979a45824f3c7dbec64ceafb228960f3333df
gimhantharuke456 <gimhantharuke456@gmail.com>
2024-09-27 19:30:58 +0530

    ADD:inquiry mail feature done

--- backend/modules/inquiry/service.js
+++ backend/modules/inquiry/service.js
@@ -1,7 +1,13 @@
+const sendMail = require("../mails/mailsend");
 const Inquiry = require("./model");
 
 class InquiryService {
   async createInquiry(inquiryData) {
+    sendMail(
+      inquiryData.email,
+      "Inquiry Created",
+      "Dear customer, we recived your inquiery, we will get back to you soon"
+    );
     return await Inquiry.create(inquiryData);
   }
 
